33b602d81f71cf8f8cb56e901ce699011b561d3c
Diego Cuevas <diegocuevas579@gmail.com>
2022-06-22 01:07:51 -0500

    ADD: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+author1 = Author.create(name: "Diego")
+book1 = Book.create(name: "book1", publish_at: "12/12/2001", author_id: author1.id)
+book2 = Book.create(name: "book2", publish_at: "12/01/2001", author_id: author1.id)
+book3 = Book.create(name: "book3", publish_at: "12/05/2001", author_id: author1.id)
