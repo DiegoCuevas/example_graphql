fa4829670d79a9fb1108a43244e0320da4812784
Diego Cuevas <diegocuevas579@gmail.com>
2022-06-22 01:26:08 -0500

    add: graphql files

---
+++ app/graphql/example_graphql_schema.rb
@@ -0,0 +1,16 @@
+class ExampleGraphqlSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+
+  use GraphQL::Dataloader
+
+  # GraphQL-Ruby calls this when something goes wrong while running a query:
+  def self.type_error(err, context)
+    # if err.is_a?(GraphQL::InvalidNullError)
+    #   # report to your bug tracker here
+    #   return nil
+    # end
+    super
+  end
+
+end
